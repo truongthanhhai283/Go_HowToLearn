45e22acb71ccec9b4b7adc4ff95ffd8b7e75fc09
truongthanhhai283 <truongthanhhai283@gmail.com>
2021-01-24 18:02:31 +0700

    Closure

---
+++ src/github.com/truongthanhhai283/Go_HowToLearn/6. Functions/l. Closure/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	a:=incrementor()
+	b:=incrementor()
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(b())
+}
+
+func incrementor() func() int {
+	var x int
+	return func() int {
+		x++
+		return x
+	}
+}
