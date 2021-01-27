6ad89066ea12f887b7db584c7fd3490b3dcbfd3a
truongthanhhai283 <truongthanhhai283@gmail.com>
2021-01-27 21:25:39 +0700

    Directional channels

---
+++ src/github.com/truongthanhhai283/Go_HowToLearn/10. Channels/b. Directional channels/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int)
+	cr := make(<-chan int) //receive
+	cs := make(chan<- int) //send
+
+	fmt.Printf("%T\n", c)
+	fmt.Printf("%T\n", cr)
+	fmt.Printf("%T\n", cs)
+}
